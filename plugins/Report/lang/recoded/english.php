7d7827940c64046aee7680b351c4fad6c54d2cae
Leonardo Sapiras <l.sapiras@gmail.com>
2011-08-15 13:19:37 -0300

    Report lang directory

---
+++ plugins/Report/lang/recoded/english.php
@@ -0,0 +1,12 @@
+<?php
+
+	/**
+	 * English language file for plugin Example.  Use this as a basis
+	 * for new translations.
+	 */
+
+	// Language and character set
+	$lang['appcharset'] = 'ISO-8859-1';
+
+	$plugin_lang['strplugindescription'] = 'Report plugin';
+?>
