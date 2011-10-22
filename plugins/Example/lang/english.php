c89896d3f24d80c96e31be2e01426d63a786d0dc
Jehan-Guillaume (ioguix) de Rorthais <ioguix@free.fr>
2011-10-22 10:16:50 +0200

    Fix Example plugin strings

--- plugins/Example/lang/english.php
+++ plugins/Example/lang/english.php
@@ -6,28 +6,28 @@
 	 */
 
 	// Language and character set
-	$lang['appcharset'] = 'ISO-8859-1';
+	$plugin_lang['appcharset'] = 'ISO-8859-1';
 
 	//Plugin data
-	$lang['strdescription'] = 'Plugin Example';
+	$plugin_lang['strdescription'] = 'Plugin Example';
 
 	//Links strings
-	$lang['strlinktoplevel'] = 'Example';
-	$lang['strlinklevel1'] = 'Level 1';
-	$lang['strlinklevel2'] = 'Level 2';
-	$lang['strlinklevel3'] = 'Level 3';
-	$lang['strlinklevel4'] = 'Level 4';
-	$lang['strdisplayext'] = 'Display extension';
-	$lang['strdbext'] = 'Databases extension';
-	$lang['strextraaction'] = 'Extra action';
-	$lang['strlinklevel2s1'] = 'Level 2.1';
-	$lang['strlinklevel2s2'] = 'Level 2.2';
+	$plugin_lang['strlinktoplevel'] = 'Example';
+	$plugin_lang['strlinklevel1'] = 'Level 1';
+	$plugin_lang['strlinklevel2'] = 'Level 2';
+	$plugin_lang['strlinklevel3'] = 'Level 3';
+	$plugin_lang['strlinklevel4'] = 'Level 4';
+	$plugin_lang['strdisplayext'] = 'Display extension';
+	$plugin_lang['strdbext'] = 'Databases extension';
+	$plugin_lang['strextraaction'] = 'Extra action';
+	$plugin_lang['strlinklevel2s1'] = 'Level 2.1';
+	$plugin_lang['strlinklevel2s2'] = 'Level 2.2';
 
 	//Pages
-	$lang['strdesclevel2'] = 'This is the level 2';
-	$lang['strdesclevel3'] = 'This is the level 3';
-	$lang['strdesclevel4'] = 'This is the level 4';
-	$lang['strschemaext'] = 'Schema extention';
+	$plugin_lang['strdesclevel2'] = 'This is the level 2';
+	$plugin_lang['strdesclevel3'] = 'This is the level 3';
+	$plugin_lang['strdesclevel4'] = 'This is the level 4';
+	$plugin_lang['strschemaext'] = 'Schema extention';
 
 	
 ?>
--- plugins/Example/lang/portuguese-br.php
+++ plugins/Example/lang/portuguese-br.php
@@ -6,27 +6,27 @@
 	 */
 
   // Language and character set
-	$lang['appcharset'] = 'ISO-8859-1';
+	$plugin_lang['appcharset'] = 'ISO-8859-1';
 
 	//Dados do plugin
-	$lang['strdescription'] = 'Plugin Exemplo';
+	$plugin_lang['strdescription'] = 'Plugin Exemplo';
 
 	//Links
-	$lang['strlinktoplevel'] = 'Exemplo';
-	$lang['strlinklevel1'] = 'Nível 1';
-	$lang['strlinklevel2'] = 'Nível 2';
-	$lang['strlinklevel3'] = 'Nível 3';
-	$lang['strlinklevel4'] = 'Nível 4';
-	$lang['strdisplayext'] = 'Extensão de exibição';
-	$lang['strdbext'] = 'Extensão das bases de dados';
-	$lang['strextraaction'] = 'Ação extra';
-	$lang['strlinklevel2s1'] = 'Nível 2.1';
-	$lang['strlinklevel2s2'] = 'Nível 2.2';
+	$plugin_lang['strlinktoplevel'] = 'Exemplo';
+	$plugin_lang['strlinklevel1'] = 'Nível 1';
+	$plugin_lang['strlinklevel2'] = 'Nível 2';
+	$plugin_lang['strlinklevel3'] = 'Nível 3';
+	$plugin_lang['strlinklevel4'] = 'Nível 4';
+	$plugin_lang['strdisplayext'] = 'Extensão de exibição';
+	$plugin_lang['strdbext'] = 'Extensão das bases de dados';
+	$plugin_lang['strextraaction'] = 'Ação extra';
+	$plugin_lang['strlinklevel2s1'] = 'Nível 2.1';
+	$plugin_lang['strlinklevel2s2'] = 'Nível 2.2';
 
 	//Paginas
-	$lang['strdesclevel2'] = 'Este é o nível 2';
-	$lang['strdesclevel3'] = 'Este é o nível 3';
-	$lang['strdesclevel4'] = 'Este é o nível 4';
-	$lang['strschemaext'] = 'Extensão de schemas';
+	$plugin_lang['strdesclevel2'] = 'Este é o nível 2';
+	$plugin_lang['strdesclevel3'] = 'Este é o nível 3';
+	$plugin_lang['strdesclevel4'] = 'Este é o nível 4';
+	$plugin_lang['strschemaext'] = 'Extensão de schemas';
 
 ?>
